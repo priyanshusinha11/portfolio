4b0b4f76ad5624b2837e66b334ffe86cd44d1cde
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2024-09-03 15:39:16 +0530

    added background beams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@
 
 export default function Home() {
   return (
-    <h1>Hello from Priyanshu's Portfolio</h1>
+    <h1 className="text-center text-gray-500 text-xl">Hello from Priyanshu's Portfolio</h1>
   );
 }
