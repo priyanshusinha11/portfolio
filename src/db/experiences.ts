d2195bbd85d9d4b74c634a68fd1293fd99dc977a
Priyanshu Sinha <priyanshusinha.2807@gmail.com>
2024-09-03 20:47:07 +0530

    exp changes

--- src/db/experiences.ts
+++ src/db/experiences.ts
@@ -3,15 +3,16 @@ const experiences = [
         company: "Saptang Labs",
         role: "Fullstack SDE Intern",
         work: [
-            "Researched and blacklisted malicious IPs, enhancing cybersecurity against threats like DDoS and malware.",
-            "Integrated ModSecurity with Nginx, using OWASP CRS for robust backend security.",
-            "Automated data management and web scraping with Python, optimizing JSON file organization",
-
+            "Conducted comprehensive IP research and blacklisting, utilizing advanced mathematical techniques to identify and mitigate threats from IP addresses associated with TOR, VPNs, proxies, and cloud hosting services.",
+            "Enhanced cybersecurity by investigating and blacklisting IPs involved in cyber attacks, including DDoS, malware, and ransomware, contributing to a more secure network environment.",
+            "Integrated ModSecurity with Nginx, effectively setting up a web application firewall (WAF) with the OWASP Core Rule Set (CRS) to provide a baseline of robust security measures against various web threats.",
+            "Explored and implemented Nginx features such as reverse proxy, load balancing, and API routing, optimizing server performance and reliability in handling network traffic.",
+            "Developed and deployed advanced bot detection techniques, including session-based analysis and mouse movement profiling, to prevent malicious bot activities and enhance overall system integrity.",
+            "Automated data management tasks and web scraping processes using Python, including crafting a Selenium-based web scraper and optimizing JSON data organization to streamline data handling and improve operational efficiency."
         ],
         joining_date: "March 2024",
         ending_date: "June 2024"
     },
-
 ]
 
 export default experiences;
